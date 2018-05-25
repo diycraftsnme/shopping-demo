e1533473bd0afbc48250e0d620538c178cfc4703
diycraftsnme <diycraftsnme@gmail.com>
2018-05-24 23:41:42 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+// app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.redirect('/index.html');
+});
+
+var PORT = process.env.PORT || 8080;
+app.listen(PORT);
