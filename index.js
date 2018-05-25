2343d59b3c4a7f107a5e19da36345628f3bba7f8
diycraftsnme <diycraftsnme@gmail.com>
2018-05-24 23:59:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 app.get('/', function(req, res){
-  res.redirect('index');
+  res.redirect('/index.html');
 });
 
 var PORT = process.env.PORT || 8080;
