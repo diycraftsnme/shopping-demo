1d9365100114c66ee79cda0e735b0b8fe3b9d29c
MuthuNithya <muthukrishnan.suresh1987@gmail.com>
2018-05-25 01:55:15 -0700

    Commit

--- src/script/app.js
+++ src/script/app.js
@@ -102,7 +102,7 @@ $(document).ready(function () {
 
 
         $scope.openQuickView = function (id) {
-            var elem = new Foundation.Reveal(angular.element('#quickView_Modal_'+id));
+            var elem_id = new Foundation.Reveal(angular.element('#quickView_Modal_'+id));
             dpc.currentProdId = id;
             angular.element('#quickView_Modal_'+id).foundation('open');
         };
@@ -120,6 +120,7 @@ $(document).ready(function () {
                         $scope.SaveState($scope.cartDetails);
                         angular.element('.cart-count').text($scope.cartDetails.length);
                         angular.element('#quickView_Modal_'+pid).foundation('close');
+                        angular.element('#quickView_Modal_'+pid).foundation('_destroy');
                         angular.element('.mini-cart').addClass('show').stop().delay(1000).queue(function(){
                             angular.element(this).removeClass('show');
                         });
@@ -150,24 +151,69 @@ $(document).ready(function () {
 
         if($state.current.name == 'home' || $state.current.name == '/'){
             //var elem = new Foundation.Orbit(angular.element('.orbit'));
+            angular.element('body').addClass('homepage').removeClass('categorypage productpage sofacategory  chaircategory tablecategory');
+            $scope.productArray = {
+                "Sofa":[
+                    {"id":"sofa_001","image":"src/Images/Sofa-new-1.jpg","name":"Sofa Deluxe","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"2500.00","offerprice":"2000.00","promotion":"Get INR500 OFF"},
+                    {"id":"sofa_002","image":"src/Images/Sofa-new-2.jpg","name":"Sofa Modern","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"3500.00","offerprice":"3500.00","promotion":""}
+                ],
+                "Chair":[
+                    {"id":"chair_001","image":"src/Images/chair-new-1.jpg","name":"Chair Deluxe","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"700.00","offerprice":"600.00","promotion":"Get INR100 OFF"},
+                    {"id":"chair_002","image":"src/Images/Chair-new-2.jpg","name":"Chair Comfort","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"1500.00","offerprice":"1500.00","promotion":""},
+                    {"id":"chair_003","image":"src/Images/chair-new-3.jpg","name":"Express Chair","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"550.00","offerprice":"450.00","promotion":"Get INR100 OFF"},
+                    {"id":"chair_004","image":"src/Images/Chair-new-4.jpg","name":"Comfort Chair","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"550.00","offerprice":"450.00","promotion":"Get INR100 OFF"},
+                    {"id":"chair_005","image":"src/Images/Chair-new-5.jpg","name":"Extreme comfort Chair","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"550.00","offerprice":"450.00","promotion":"Get INR100 OFF"}
+
+                ],
+                "Table":[{"id":"table_004","image":"src/Images/table-new-1.jpg","name":"Kitchen Table","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"1050.00","offerprice":"950.00","promotion":"Get INR100 OFF"},
+                    {"id":"table_001","image":"src/Images/table-new-2.jpg","name":"Luxury Table","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"1050.00","offerprice":"950.00","promotion":"Get INR100 OFF"},
+                    {"id":"table_002","image":"src/Images/table-new-3.jpg","name":"Deluxe Table","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"1050.00","offerprice":"950.00","promotion":"Get INR100 OFF"},
+                    {"id":"table_003","image":"src/Images/table-new-4.jpg","name":"Express Table","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"1050.00","offerprice":"950.00","promotion":"Get INR100 OFF"}
+                ]
+            };
         } else if($state.current.name == 'category'){
             $scope.largeGridCount = 3;
             $scope.mediumGridCount = 2;
             $state.categoryName = $state.params.cid;
+            angular.element('body').addClass('categorypage').removeClass('homepage productpage');
             if($state.categoryName === 'sofas'){
+                angular.element('body').addClass('sofacategory').removeClass('chaircategory tablecategory');
                 $scope.categoryProducts = $scope.productArray.Sofa;
                 $scope.currentCategoryName = 'Sofas';
                 $scope.currentCategoryImage = 'src/Images/sofa4.jpg';
+                $scope.productArray = {
+                    "Sofa":[
+                        {"id":"sofa_001","image":"src/Images/Sofa-new-1.jpg","name":"Sofa Deluxe","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"2500.00","offerprice":"2000.00","promotion":"Get INR500 OFF"},
+                        {"id":"sofa_002","image":"src/Images/Sofa-new-2.jpg","name":"Sofa Modern","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"3500.00","offerprice":"3500.00","promotion":""}
+                    ]};
             } else if($state.categoryName === 'chairs'){
+                angular.element('body').addClass('chaircategory').removeClass('sofacategory  tablecategory');
                 $scope.categoryProducts = $scope.productArray.Chair;
                 $scope.currentCategoryName = 'Chairs';
                 $scope.currentCategoryImage = 'src/Images/chair4.jpg';
+                $scope.productArray = {"Chair":[
+                    {"id":"chair_001","image":"src/Images/chair-new-1.jpg","name":"Chair Deluxe","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"700.00","offerprice":"600.00","promotion":"Get INR100 OFF"},
+                    {"id":"chair_002","image":"src/Images/Chair-new-2.jpg","name":"Chair Comfort","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"1500.00","offerprice":"1500.00","promotion":""},
+                    {"id":"chair_003","image":"src/Images/chair-new-3.jpg","name":"Express Chair","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"550.00","offerprice":"450.00","promotion":"Get INR100 OFF"},
+                    {"id":"chair_004","image":"src/Images/Chair-new-4.jpg","name":"Comfort Chair","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"550.00","offerprice":"450.00","promotion":"Get INR100 OFF"},
+                    {"id":"chair_005","image":"src/Images/Chair-new-5.jpg","name":"Extreme comfort Chair","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"550.00","offerprice":"450.00","promotion":"Get INR100 OFF"}
+
+                ]};
             } else{
+                angular.element('body').addClass('tablecategory').removeClass('sofacategory chaircategory');
                 $scope.categoryProducts = $scope.productArray.Table;
                 $scope.currentCategoryName = 'Tables';
                 $scope.currentCategoryImage = 'src/Images/table2.jpg';
+                $scope.productArray = {
+                    "Table":[{"id":"table_004","image":"src/Images/table-new-1.jpg","name":"Kitchen Table","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"1050.00","offerprice":"950.00","promotion":"Get INR100 OFF"},
+                        {"id":"table_001","image":"src/Images/table-new-2.jpg","name":"Luxury Table","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"1050.00","offerprice":"950.00","promotion":"Get INR100 OFF"},
+                        {"id":"table_002","image":"src/Images/table-new-3.jpg","name":"Deluxe Table","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"1050.00","offerprice":"950.00","promotion":"Get INR100 OFF"},
+                        {"id":"table_003","image":"src/Images/table-new-4.jpg","name":"Express Table","description":"Elegant and compact design. Easy-to-clean faux leather covering.","originalprice":"1050.00","offerprice":"950.00","promotion":"Get INR100 OFF"}
+                    ]
+                };
             }
         } else if($state.current.name == 'product'){
+            angular.element('body').addClass('productpage').removeClass('homepage categorypage sofacategory  chaircategory tablecategory');
             var pid = $state.params.pid;
                 $state.categoryName = $state.params.cid.toLowerCase();
 
@@ -178,7 +224,7 @@ $(document).ready(function () {
                         $scope.productDetails = $scope.categoryProducts[i];
                     }
                 }
-                $scope.currentCategoryName = 'Sofas';
+                $scope.currentCategoryName = 'sofas';
             } else if($state.categoryName === 'chairs'){
                 $scope.categoryProducts = $scope.productArray.Chair;
                 for(var i=0;i<$scope.categoryProducts.length;i++){
@@ -186,7 +232,7 @@ $(document).ready(function () {
                         $scope.productDetails = $scope.categoryProducts[i];
                     }
                 }
-                $scope.currentCategoryName = 'Chairs';
+                $scope.currentCategoryName = 'chairs';
             } else{
                 $scope.categoryProducts = $scope.productArray.Table;
                 for(var i=0;i<$scope.categoryProducts.length;i++){
@@ -194,7 +240,7 @@ $(document).ready(function () {
                         $scope.productDetails = $scope.categoryProducts[i];
                     }
                 }
-                $scope.currentCategoryName = 'Tables';
+                $scope.currentCategoryName = 'tables';
             }
         }
 
